c14700bfca84c97fb49ea00787405823a8c11e13
krishsat <88029295+krishsat@users.noreply.github.com>
2022-04-19 10:57:57 +1000

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,34 @@
+//YOUR FIREBASE LINKS
+const firebaseConfig = {
+      apiKey: "AIzaSyCst-LjOyQ1Ly3bUK2qeZaSvMCjVlh21Dk",
+      authDomain: "kwitter-ecaef.firebaseapp.com",
+      projectId: "kwitter-ecaef",
+      storageBucket: "kwitter-ecaef.appspot.com",
+      messagingSenderId: "792869021734",
+      appId: "1:792869021734:web:b86cab3ac951d5703f5ee7"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    user_name = localStorage.getItem("user_name");
+    room_name = localStorage.getItem("room_name");
+
+    function send(){
+          msg = document.getElementById("msg").value;
+          firebase.database().ref(room_name).push({
+                name:user_name, 
+                message:msg, 
+                like:0
+          });
+          document.getElementById("msg").value = "";
+    }
+
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+//Start code
+
+//End code
+      } });  }); }
+getData();
